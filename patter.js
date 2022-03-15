db8af62e0ca31601a92e7aaba46631e93f29a8fc
klownfish <github@clownfish.xyz>
2022-03-15 14:28:02 +0100

    high quality pat

--- patter.js
+++ patter.js
@@ -25,34 +25,41 @@ class Patter {
             quality: 10
         });
         let avatar_img = await loadImage(avatar);
+        
+        let avatar_x = 15
+        let avatar_y = 15
+        let avatar_width = 150
+        let avatar_height = 150
+        let image_width = 180
+        let image_height = 165
+        let hand_x = 0
+        let hand_y = 0
 
-        let avatar_offset_x = 15
-        let avatar_offset_y = 15
-        let canvas = createCanvas(150 + avatar_offset_x, 150 + avatar_offset_y);
+        let canvas = createCanvas(image_width, image_height);
         let ctx = canvas.getContext("2d");
 
         let y_scale = [
             1,
-            0.9,
-            0.8,
-            0.9,
+            0.85,
+            0.7,
+            0.85,
             1
         ]
 
         let x_scale = [
-            0.9,
-            0.95,
+            0.85,
+            0.90,
             1,
-            0.95,
-            0.9
+            0.90,
+            0.85
         ]
 
         let hand_offset = [0, 0];
-        let delay = 50;
+        let delay = 55;
         ctx.fillStyle = "#36393E"
         for (let i = 0; i < 5; i++) {
-            ctx.fillRect(0, 0, 150 + avatar_offset_x, 150 + avatar_offset_y)
-            ctx.drawImage(avatar_img, 0, 0, avatar_img.width, avatar_img.height, (1 - x_scale[i]) * 150 + avatar_offset_x, (1 - y_scale[i]) * 150 + avatar_offset_y, 150 * x_scale[i], 150 * y_scale[i]);
+            ctx.fillRect(0, 0, image_width, image_height)
+            ctx.drawImage(avatar_img, 0, 0, avatar_img.width, avatar_img.height, (1 - x_scale[i]) * avatar_width / 2 + avatar_x, (1 - y_scale[i]) * avatar_height + avatar_y, avatar_width * x_scale[i], avatar_height * y_scale[i]);
             ctx.drawImage(this.hand_frames[i], ...hand_offset);
             gif.addFrame(ctx.getImageData(0, 0, canvas.width, canvas.height), {delay: delay})
         }
